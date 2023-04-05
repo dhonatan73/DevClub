315624f4039c0d524e2b324587f40d2eb11fdb1f
dhonatan73 <dhonatanfigueiredo73@gmail.com>
2023-04-05 20:04:23 -0300

    Primeiro commit

---
+++ Site de carro/scripts.js
@@ -0,0 +1,13 @@
+ function trocarMarca (marca) {
+    let imgmarca = document.querySelector(".volkswagem")
+    imgmarca.src = marca
+    
+}
+
+
+
+function trocargti (cor) {
+    let imgcar = document.querySelector(".imagem-carro")
+    imgcar.src = cor
+    
+}
